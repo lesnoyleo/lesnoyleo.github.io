bbd07948a38607784209af2bcec342ecd9a962e6
lesnoy <lesnoy@DESKTOP-7LOKC8J>
2020-06-30 23:34:52 +0300

    test

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-$(document).ready(function(){
-    $("#menu").on("click","a", function (event) {
-        //отменяем стандартную обработку нажатия по ссылке
-        event.preventDefault();
+﻿
+if (window.DeviceMotionEvent == undefined) {
+	//No accelerometer is present. Use buttons. 
+	document.querySelector("#acc").textContent = "NO";
+	document.querySelector("#acc").className = "no";
 
-        //забираем идентификатор бока с атрибута href
-        var id  = $(this).attr('href'),
+}
+else {
+	document.querySelector("#acc").textContent = "YES";
+	document.querySelector("#acc").className = "yes";
+	window.addEventListener("devicemotion", accelerometerUpdate, true);
+}
 
-        //узнаем высоту от начала страницы до блока на который ссылается якорь
-            top = $(id).offset().top;
-        
-        //анимируем переход на расстояние - top за 1500 мс
-        $('body,html').animate({scrollTop: top}, 1500);
-        });
-    });
+
+function accelerometerUpdate(event) {
+   var aX = event.accelerationIncludingGravity.x*10;
+   var aY = event.accelerationIncludingGravity.y*10;
+   var aZ = event.accelerationIncludingGravity.z*10;
+
+	document.querySelector("#x").value = aX;
+	document.querySelector("#y").value = aY;
+	document.querySelector("#z").value = aZ;
+
+	// ix aY is negative, switch rotation
+	if (aY <0) {
+		aX = -aX - 180;
+	}
+	document.querySelector("#block").style.transform="rotate("+aX+"deg)";
+
+}
